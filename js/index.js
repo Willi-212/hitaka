9076612692be1b87cf98db89736e2e30d986d641
Leo <rocket.mark10@protonmail.com>
2022-11-14 15:08:46 +0100

    Update

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// nav barr respo
+let list = document.querySelector('#list');
+let humb = document.querySelector('#humb');
+
+humb.addEventListener('click',()=>{
+  list.classList.toggle("show");
+})
--- js/main.js
+++ js/main.js
@@ -16,3 +16,52 @@ var somme =document.getElementById('somme').innerHTML ;
 
  }
       ;
+
+// filer
+let cards = document.querySelectorAll(".box");
+let plat = document.querySelector("#btn-plat");
+let salade = document.querySelector("#btn-salade");
+let obento = document.querySelector("#btn-obento");
+let all = document.querySelector("#btn-all");
+let sushi = document.querySelector("#btn-sushi");
+
+sushi.addEventListener('click',()=>{
+  cards.forEach((element)=>{
+    element.style.display = "block";
+    if(!element.classList.contains("sushi")){
+      element.style.display = "none";
+    }
+  })
+})
+plat.addEventListener('click',()=>{
+  cards.forEach((element)=>{
+    element.style.display = "block";
+    if(!element.classList.contains("plats")){
+      element.style.display = "none";
+    }
+  })
+})
+salade.addEventListener('click',()=>{
+  cards.forEach((element)=>{
+    element.style.display = "block";
+    if(!element.classList.contains("salade")){
+      element.style.display = "none";
+    }
+  })
+})
+obento.addEventListener('click',()=>{
+  cards.forEach((element)=>{
+    element.style.display = "block";
+    if(!element.classList.contains("obento")){
+      element.style.display = "none";
+    }
+  })
+})
+
+// nav barr respo
+let list = document.querySelector('#list');
+let humb = document.querySelector('#humb');
+
+humb.addEventListener('click',()=>{
+  list.classList.toggle("show");
+})
