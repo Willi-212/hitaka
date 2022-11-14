8f558c754500c4e7e952ec794f91dfb57f8585e6
younes <rocket.mark10@protonmail.com>
2022-11-13 20:22:09 -0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// alert("Test")
+var total = 0;
+ 
+function Calculate(item){
+       if(item.checked){
+        total+= parseInt(item.value);
+           
+       }else{
+          total-= parseInt(item.value);
+       }
+       document.getElementById('somme').innerHTML = total;
+   };
+function recupValeurs() {
+
+var somme =document.getElementById('somme').innerHTML ;
+  alert("la somme des choix selectionnees est : "+ somme);
+
+ }
+      ;
